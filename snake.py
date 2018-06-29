7dfad5443515aa3438be54f34785a2b7abeb1bc8
Christopher Hurt <topherhurt@gmail.com>
2018-06-29 10:56:50 -0400

    Added snake movement

--- snake.py
+++ snake.py
@@ -1,18 +1,24 @@
 from pygame.locals import *
+from block import Block
 
 class Snake:
     def __init__(self):
-        self.left = 10
-        self.top = 25
-        self.width = 10
-        self.height = 10
-        self.color = (250, 0, 0)
-        self.dx = 0
+        self.body = [Block(10, 20), Block(20, 20), Block(20, 30)]
+        self.color = (0, 250, 0)
+        self.dx = 10
         self.dy = 0
 
     def move(self):
-        self.left += self.dx
-        self.top += self.dy
+        for i, _ in enumerate(self.body[:-1]):
+            self.body[i].left = self.body[i+1].left
+            self.body[i].top = self.body[i+1].top
+        self.body[-1].left += self.dx
+        self.body[-1].top += self.dy
 
-    def get_rect(self):
-        return Rect(self.left, self.top, self.width, self.height)
+    def get_rect(self, i):
+        body = self.body
+        return Rect(body[i].left, body[i].top, body[i].width, body[i].height)
+
+    def add_block(self):
+        lastblock = self.body[-1]
+        self.body.insert(0, Block(lastblock.left, lastblock.top))
