0bddb410d06c1d634dc17d4d8526aac913e8c2d0
Christopher Hurt <topherhurt@gmail.com>
2018-07-02 14:08:42 -0400

    Separate collision logic from snake object

--- snake.py
+++ snake.py
@@ -5,6 +5,7 @@ class Snake:
     def __init__(self):
         # The tail is the first index, the head is the last index
         self.body = [Block(10, 20)]
+        self.size = 1
         # Keep track of where the next tail will appear
         self.next_tail = Block(0, 0)
         self.color = (0, 250, 0)
@@ -23,66 +24,35 @@ class Snake:
         self.body[-1].top += self.dy
 
     def move_left(self):
-        if not self.dx == 10:
+        # Snakes can't go in reverse if longer than 1 block
+        if not self.dx == 10 or self.size < 2:
             self.dx = -10
             self.dy = 0
 
     def move_up(self):
-        if not self.dy == 10:
+        # Snakes can't go in reverse if longer than 1 block
+        if not self.dy == 10 or self.size < 2:
             self.dx = 0
             self.dy = -10
 
     def move_right(self):
-        if not self.dx == -10:
+        # Snakes can't go in reverse if longer than 1 block
+        if not self.dx == -10 or self.size < 2:
             self.dx = 10
             self.dy = 0
 
     def move_down(self):
-        if not self.dy == -10:
+        # Snakes can't go in reverse if longer than 1 block
+        if not self.dy == -10 or self.size < 2:
             self.dx = 0
             self.dy = 10
 
     def get_rect(self, i):
-        # Return a rectangle of the desired body
+        # Return a rectangle of the desired body part
         body = self.body
         return Rect(body[i].left, body[i].top, body[i].width, body[i].height)
 
     def add_tail(self):
         # Add a tail block to beginning of list / end of snake
         self.body.insert(0, self.next_tail)
-
-    def did_collide_with_wall(self, screensize):
-        screen_w = screensize[0]
-        screen_h = screensize[1]
-        head = self.body[-1]
-        # Check left side of screen
-        if head.left+head.width < 0:
-            return True
-        # Check top of screen
-        if head.top+head.height < 0:
-            return True
-        # Check right side of screen
-        if head.left+head.width > screen_w:
-            return True
-        # Check bottom of screen
-        if head.top+head.height > screen_h:
-            return True
-        # Snake is still in the play area
-        return False
-
-    def did_collide_with_body(self):
-        head = self.body[-1]
-        head_x = head.left + (0.5 * head.width)
-        head_y = head.top + (0.5 * head.height)
-        for _,block in enumerate(self.body[:-1]):
-            if head_x >= block.left and head_x <= block.left+block.width and head_y >= block.top and head_y <= block.top+block.height:
-                return True
-        return False
-
-    def did_collide_with_food(self, food):
-        head = self.body[-1]
-        head_xpos = head.left + (0.5 * head.width)
-        head_ypos = head.top + (0.5 * head.height)
-        if head_xpos >= food.left and head_xpos <= food.left+food.width and head_ypos >= food.top and head_ypos <= food.top+food.height:
-            return True
-        return False
+        self.size += 1
